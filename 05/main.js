d4d290aa3cef1b9333e09ae945fb6e62e84871ab
emiliano gonzalez <piwi1589@gmail.com>
2023-03-14 21:09:36 -0300

    esaa

---
+++ 05/main.js
@@ -0,0 +1,106 @@
+/*
+
+const persona1 = {
+    nombre : 'Emiliano',
+    edad : 33,
+    calle : 'Falsa 123'
+
+}
+
+console.log(persona1)
+*/
+
+
+
+
+//2
+
+/*
+function Persona(nombre,edad,calle){
+    this.nombre = nombre;
+    this.edad = edad;
+    this.calle = calle;
+}
+*/
+
+
+
+
+//Clases
+
+/*
+class Persona {
+    constructor(nombre, edad, calle){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.calle = calle;
+    }
+}
+const persona1 = new Persona('Emiliano', 33, 'Calle falsa 123');
+const persona2 = new Persona('Marcelo', 32, 'Isla de gorriti 5541');
+
+console.log(persona1)
+console.log(persona2)
+*/
+
+
+
+// otro ejemplo
+
+
+/*
+class Producto{
+    constructor(nombre, precio){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.vendido = false;
+    }
+    vender(){
+        this.vendido = true;
+    }
+    iva(){
+        return this.precio * 1.21
+    }
+}
+
+const producto = new Producto('ps5', 150000)
+console.log(producto.vendido)
+producto.vender()
+console.log(producto.vendido)
+*/
+
+class Cliente{
+    constructor(nombre, direccion){
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    descripcion(){
+        return this.nombre+' con direccion en '+ this.direccion
+    }
+}
+
+class Pizza{
+    constructor(tipoQueso, agregado, aceituna, cliente){
+        this.tipoQueso = tipoQueso;
+        this.agregado = agregado;
+        this.aceituna = aceituna;
+        this.cliente = cliente;
+    }
+    cocinar(){
+        return 'Ya esta cocinada la pizza'
+    }
+    entregar(){
+        return 'La pizza va a ser entregada a: '+ this.cliente.descripcion()
+    }
+
+}
+
+const objCliente = new Cliente('Emiliano', "calle falsa 123")
+const objPizza = new Pizza('muzza','jamon y anana',false,objCliente)
+
+objPizza.cocinar()
+objPizza.entregar()
+
+console.log(objPizza.cocinar())
+console.log(objPizza.entregar())
--- PreEntrega1_GonzalezEmiliano/main.js
+++ PreEntrega1_GonzalezEmiliano/main.js
@@ -4,31 +4,33 @@ do {
 
     let name = prompt('Hola, ingresa tu nombre por favor: ')
     let choice = prompt('Bienvenido a nuestra tienda ' + name + '. Por favor selecciona un articulo: \n 1) Teclado USD140 \n 2) Mouse USD67  \n 3) Monitor USD415 \n 4) Tarjeta de video USD899 \n O presiona ESC para salir.')
-    do {
+
+    while (choice != '7') {
+        
         switch (choice) {
 
             case '1':
                 alert('Teclado anadido al carrito, USD 140')
-                carrito=carrito + 140
+                carrito = carrito + 140
                 choice = prompt('Desea seguir comprando? \n 6) Si \n 7) No')
                 break;
 
             case '2':
                 alert('Mouse anadido al carrito, USD 67')
-                carrito=carrito +67
+                carrito = carrito + 67
                 choice = prompt('Desea seguir comprando? \n 6) Si \n 7) No')
                 break;
 
             case '3':
                 alert('Monitor anadido al carrito, USD 415')
-                carrito=carrito +415
+                carrito = carrito + 415
                 choice = prompt('Desea seguir comprando? \n 6) Si \n 7) No')
 
                 break;
 
             case '4':
                 alert('Tarjeta de video anadido al carrito, USD 899')
-                carrito=carrito + 899
+                carrito = carrito + 899
                 choice = prompt('Desea seguir comprando? \n 6) Si \n 7) No')
 
                 break;
@@ -43,19 +45,13 @@ do {
                 choice = prompt('Desea seguir comprando? \n 6) Si \n 7) No')
 
                 break;
-            
-            case '7':
-                alert("Total USD: " + carrito)
-    
-                break;
 
             default:
                 break;
         }
+        alert('Total USD: ' + carrito)
 
-    } while (choice != 9);
-    alert("Total USD: " + carrito)
-
+    }
 
 
 } while (choice != "ESC")
