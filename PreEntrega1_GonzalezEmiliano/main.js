328a999642889040a4e425084ea37f665c6a4280
emiliano gonzalez <piwi1589@gmail.com>
2023-03-30 21:03:33 -0300

    niceee

--- PreEntrega1_GonzalezEmiliano/main.js
+++ PreEntrega1_GonzalezEmiliano/main.js
@@ -1,57 +1,49 @@
-let carrito = 0
-
 do {
 
     let name = prompt('Hola, ingresa tu nombre por favor: ')
-    let choice = prompt('Bienvenido a nuestra tienda ' + name + '. Por favor selecciona un articulo: \n 1) Teclado USD140 \n 2) Mouse USD67  \n 3) Monitor USD415 \n 4) Tarjeta de video USD899 \n O presiona ESC para salir.')
+    let saludo = 'Bienvenido a nuestra tienda ' + name + '. \nPor favor selecciona un articulo: \n 1) Teclado USD140 \n 2) Mouse USD67  \n 3) Monitor USD415 \n 4) Tarjeta de video USD899 \n O presiona ESC para salir.'
+    let choice = ''
+    let carrito = 0
 
-    while (choice != '7') {
-        
+    while (choice !== '6' && choice !== 'ESC') {
+
+        choice = prompt(saludo)
+
         switch (choice) {
 
             case '1':
                 alert('Teclado anadido al carrito, USD 140')
                 carrito = carrito + 140
-                choice = prompt('Desea seguir comprando? \n 6) Si \n 7) No')
+                choice = prompt('Desea seguir comprando? \n5)Si \n6)No')
                 break;
 
             case '2':
                 alert('Mouse anadido al carrito, USD 67')
                 carrito = carrito + 67
-                choice = prompt('Desea seguir comprando? \n 6) Si \n 7) No')
+                choice = prompt('Desea seguir comprando? \n5)Si \n6)No')
                 break;
 
             case '3':
                 alert('Monitor anadido al carrito, USD 415')
                 carrito = carrito + 415
-                choice = prompt('Desea seguir comprando? \n 6) Si \n 7) No')
-
+                choice = prompt('Desea seguir comprando? \n5)Si \n6)No')
                 break;
 
             case '4':
                 alert('Tarjeta de video anadido al carrito, USD 899')
                 carrito = carrito + 899
-                choice = prompt('Desea seguir comprando? \n 6) Si \n 7) No')
-
+                choice = prompt('Desea seguir comprando? \n5)Si \n6)No')
                 break;
 
-            case '5':
-                alert(carrito)
-
-                break;
-
-            case '6':
-                prompt("Por favor selecciona un articulo: \n 1) Teclado USD140 \n 2) Mouse USD67  \n 3) Monitor USD415 \n 4) Tarjeta de video USD899 \n O presiona ESC para salir.")
-                choice = prompt('Desea seguir comprando? \n 6) Si \n 7) No')
-
-                break;
-
-            default:
-                break;
         }
-        alert('Total USD: ' + carrito)
 
     }
+    if (choice == '6') {
+        alert('Gracias por tu compra ' + name + '!\n' + "Total USD " + carrito + '.' + '\n ' + 'En breves te llegará un email confirmando tu compra.')
 
+    } else {
+        alert('Compra cancelada :(')
+
+    }
 
 } while (choice != "ESC")
