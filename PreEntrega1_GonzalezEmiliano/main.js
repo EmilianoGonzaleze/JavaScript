7bd2b2b71bacb27e48118398eb35a8ec73126d36
emiliano gonzalez <piwi1589@gmail.com>
2023-03-30 21:13:06 -0300

    sdasda

--- PreEntrega1_GonzalezEmiliano/main.js
+++ PreEntrega1_GonzalezEmiliano/main.js
@@ -3,7 +3,8 @@ do {
     let name = prompt('Hola, ingresa tu nombre por favor: ')
     let saludo = 'Bienvenido a nuestra tienda ' + name + '. \nPor favor selecciona un articulo: \n 1) Teclado USD140 \n 2) Mouse USD67  \n 3) Monitor USD415 \n 4) Tarjeta de video USD899 \n O presiona ESC para salir.'
     let choice = ''
-    let carrito = 0
+    let monto = 0
+
 
     while (choice !== '6' && choice !== 'ESC') {
 
@@ -13,25 +14,25 @@ do {
 
             case '1':
                 alert('Teclado anadido al carrito, USD 140')
-                carrito = carrito + 140
+                monto = monto + 140
                 choice = prompt('Desea seguir comprando? \n5)Si \n6)No')
                 break;
 
             case '2':
                 alert('Mouse anadido al carrito, USD 67')
-                carrito = carrito + 67
+                monto = monto + 67
                 choice = prompt('Desea seguir comprando? \n5)Si \n6)No')
                 break;
 
             case '3':
                 alert('Monitor anadido al carrito, USD 415')
-                carrito = carrito + 415
+                monto = monto + 415
                 choice = prompt('Desea seguir comprando? \n5)Si \n6)No')
                 break;
 
             case '4':
                 alert('Tarjeta de video anadido al carrito, USD 899')
-                carrito = carrito + 899
+                monto = monto + 899
                 choice = prompt('Desea seguir comprando? \n5)Si \n6)No')
                 break;
 
@@ -39,7 +40,7 @@ do {
 
     }
     if (choice == '6') {
-        alert('Gracias por tu compra ' + name + '!\n' + "Total USD " + carrito + '.' + '\n ' + 'En breves te llegará un email confirmando tu compra.')
+        alert('Gracias por tu compra ' + name + '!\n' + "Total USD " + monto + '.' + '\n ' + 'En breves te llegará un email confirmando tu compra.')
 
     } else {
         alert('Compra cancelada :(')
